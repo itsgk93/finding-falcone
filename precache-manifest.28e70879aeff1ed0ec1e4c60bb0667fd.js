b44f330185ba152381e17545495b62bbe8137aa6
Gaurav Katiyar <gaurav220693@gmail.com>
2021-06-16 01:33:28 +0530

    Updates

---
+++ precache-manifest.28e70879aeff1ed0ec1e4c60bb0667fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "248517448137dd5348721ebd9464137c",
+    "url": "/itsgk93/finding-falcone/index.html"
+  },
+  {
+    "revision": "2662a65c77aa8699131f",
+    "url": "/itsgk93/finding-falcone/static/css/main.ac01e043.chunk.css"
+  },
+  {
+    "revision": "a5f237b901f386dbd5fa",
+    "url": "/itsgk93/finding-falcone/static/js/2.b34c1a1d.chunk.js"
+  },
+  {
+    "revision": "2662a65c77aa8699131f",
+    "url": "/itsgk93/finding-falcone/static/js/main.8a9715c5.chunk.js"
+  },
+  {
+    "revision": "ee1b2b6c25d60ff1b04d",
+    "url": "/itsgk93/finding-falcone/static/js/runtime~main.726b844e.js"
+  }
+]);
